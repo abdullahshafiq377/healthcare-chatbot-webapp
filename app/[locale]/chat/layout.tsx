6304607ff5e46d01f4e1519445ddd3dd7a8670ce
Abdullah Shafiq <abdullahshafiq377@gmail.com>
2025-02-18 20:43:37 +0500

    auth redirects added

--- app/[locale]/chat/layout.tsx
+++ app/[locale]/chat/layout.tsx
@@ -1,11 +1,33 @@
+"use client";
+import { useRouter } from "@/i18n/routing";
+import { axiosInstance } from "@/utils/axiosInstance";
+import { useEffect } from "react";
+
 export default function ChatLayout({
-  children,
-}: {
+                                     children
+                                   }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+  const verifySession = async () => {
+    const session = await axiosInstance.get("/auth/check-session");
+
+    if (
+      !session?.data?.isAuthenticated
+    ) {
+      router.replace("/");
+    }
+    console.log("Session", session);
+  };
+
+  useEffect(() => {
+    verifySession();
+  }, []);
+
   return (
     <section className="flex flex-col h-full items-center justify-center gap-4">
-      <div className="inline-block w-full h-full text-center justify-center py-6">
+      <div
+        className="inline-block w-full h-full text-center justify-center py-6">
         {children}
       </div>
     </section>
